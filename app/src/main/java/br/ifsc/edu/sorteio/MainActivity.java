d37ba3fd003dcde4b230dd6725a35a89c388e93f
aluno <eduardo.wagner95@gmail.com>
2020-02-27 11:15:45 -0300

    mano aprende

--- app/src/main/java/br/ifsc/edu/sorteio/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/sorteio/MainActivity.java
@@ -3,6 +3,7 @@ package br.ifsc.edu.sorteio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -10,13 +11,24 @@ public class MainActivity extends AppCompatActivity {
     EditText editTextinicial, editTextfinal;
     TextView TextViewResultado;
 
+    int valorinicial, valorfinal;
+    int resultado;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editTextinicial = findViewById(R.id.editTextinicial);
-        editTextfinal =  findViewById(R.id. editTextfinal);
-        TextViewResultado =  findViewById(R.id.TextViewResultado);
+        editTextinicial = (EditText)findViewById(R.id.editTextinicial);
+        editTextfinal =  (EditText)findViewById(R.id.editTextfinal);
+        TextViewResultado = (TextView)findViewById(R.id.TextViewResultado);
+
+    }
+
+    public void Sortear(View v){
+        valorinicial = Integer.parseInt (editTextinicial.getText().toString());
+        valorfinal = Integer.parseInt(editTextfinal.getText().toString());
+        resultado = (int) ((Math.random() * (valorfinal- valorinicial))+valorinicial);
+        TextViewResultado.setText("Resultado: "+resultado);
     }
 }
