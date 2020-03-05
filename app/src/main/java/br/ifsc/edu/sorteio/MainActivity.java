ce9891fbb1483e08df3fac270f4eb6f4c24a7736
RianGustavo <rian.gustavo.a@gmail.com>
2020-03-05 10:06:32 -0300

    Sorteio pronto

--- app/src/main/java/br/ifsc/edu/sorteio/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/sorteio/MainActivity.java
@@ -5,14 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText editTextinicial, editTextfinal;
     TextView TextViewResultado;
-
+    ImageButton button;
     int valorinicial, valorfinal;
     int resultado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +25,20 @@ public class MainActivity extends AppCompatActivity {
         editTextinicial = (EditText)findViewById(R.id.editTextinicial);
         editTextfinal =  (EditText)findViewById(R.id.editTextfinal);
         TextViewResultado = (TextView)findViewById(R.id.TextViewResultado);
+        button = (ImageButton)findViewById(R.id.button);
+
 
     }
 
     public void Sortear(View v){
-        valorinicial = Integer.parseInt (editTextinicial.getText().toString());
-        valorfinal = Integer.parseInt(editTextfinal.getText().toString());
-        resultado = (int) ((Math.random() * (valorfinal- valorinicial))+valorinicial);
-        TextViewResultado.setText("Resultado: "+resultado);
+        if((editTextinicial.getText().toString().length()>0)&&(editTextfinal.getText().toString().length()>0)){
+            valorinicial = Integer.parseInt (editTextinicial.getText().toString());
+            valorfinal = Integer.parseInt(editTextfinal.getText().toString());
+            resultado = (int) ((Math.random() * (valorfinal- valorinicial))+valorinicial);
+            TextViewResultado.setText("Resultado: "+resultado);
+        }
+        else{
+            Toast.makeText(this, "Você deve preencher todos os campos para realizar o sorteio", Toast.LENGTH_SHORT).show();
+        }
     }
 }
